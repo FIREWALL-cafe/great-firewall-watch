9364755640351fa803ff22b34ee1a9a7a63a4648
Rowan Copley <dovinmu@gmail.com>
2021-02-04 15:20:55 -0800

    change back run() permanently (use test.py to run short tests instead)

--- scrape.py
+++ scrape.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
     error = False
     try:
         print(f"scraper started {datetime.utcnow()}")
-        google_img_count, baidu_img_count, total_requests = run(.005)
+        google_img_count, baidu_img_count, total_requests = run()
     except Exception as e:
         exc = traceback.format_exc()
         error = True
